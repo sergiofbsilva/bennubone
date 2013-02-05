3201f173cda97ca9863650c83bc50032dd5c7bff
sergiofbsilva <sergiofbsilva@gmail.com>
2013-02-05 02:38:00 +0000

    updated to common builder register

---
+++ src/main/java/pt/ist/bennu/coffee/manager/rest/BennuboneInitializer.java
@@ -0,0 +1,34 @@
+package pt.ist.bennu.coffee.manager.rest;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import pt.ist.bennu.bennu.core.rest.serializer.JsonAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeBatchAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeItemAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeOrderAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeOrderEntrySerializer;
+import pt.ist.bennu.coffee.manager.domain.CoffeeBatch;
+import pt.ist.bennu.coffee.manager.domain.CoffeeItem;
+import pt.ist.bennu.coffee.manager.domain.CoffeeOrder;
+import pt.ist.bennu.coffee.manager.domain.CoffeeOrderEntry;
+
+@WebListener
+public class BennuboneInitializer implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		JsonAdapter.registerTypeAdapter(CoffeeOrder.class, new CoffeeOrderAdapter());
+		JsonAdapter.registerTypeAdapter(CoffeeItem.class, new CoffeeItemAdapter());
+		JsonAdapter.registerTypeAdapter(CoffeeBatch.class, new CoffeeBatchAdapter());
+		JsonAdapter.registerTypeAdapter(CoffeeOrderEntry.class, new CoffeeOrderEntrySerializer());
+	}
+
+}
--- src/main/java/pt/ist/bennu/coffee/manager/rest/CoffeeManagerAbstractResource.java
+++ src/main/java/pt/ist/bennu/coffee/manager/rest/CoffeeManagerAbstractResource.java
@@ -1,19 +1,24 @@
 package pt.ist.bennu.coffee.manager.rest;
 
+import org.joda.time.DateTime;
+
 import pt.ist.bennu.bennu.core.rest.AbstractResource;
-import pt.ist.bennu.coffee.manager.adapter.CoffeeManagerJsonSerializer;
+import pt.ist.bennu.bennu.core.rest.serializer.JsonAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeBatchAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeItemAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeOrderAdapter;
+import pt.ist.bennu.coffee.manager.adapter.CoffeeOrderEntrySerializer;
+import pt.ist.bennu.coffee.manager.adapter.DateTimeSerializer;
+import pt.ist.bennu.coffee.manager.domain.CoffeeBatch;
+import pt.ist.bennu.coffee.manager.domain.CoffeeItem;
 import pt.ist.bennu.coffee.manager.domain.CoffeeManager;
-import pt.ist.bennu.core.domain.Bennu;
+import pt.ist.bennu.coffee.manager.domain.CoffeeOrder;
+import pt.ist.bennu.coffee.manager.domain.CoffeeOrderEntry;
 
 public abstract class CoffeeManagerAbstractResource extends AbstractResource {
 
-	static {
-		setSerializer(new CoffeeManagerJsonSerializer());
-		setDeserializer(new CoffeeManagerJsonDeSerializer());
-	}
-
 	protected CoffeeManager getCoffeeManager() {
-		return Bennu.getInstance().getCoffeeManager();
+		return CoffeeManager.getInstance();
 	}
 
 }
